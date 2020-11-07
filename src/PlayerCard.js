af19a34fb68afb366bdd2977053ade7f61400332
jonathan gatti <jonathangatti2@gmail.com>
2020-11-07 17:17:10 +0000

    fetch rating for each player

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -12,20 +12,20 @@ function PlayerCard(props){
   useEffect(() => {
     async function getData(url){
       try {
-        let res = await axios.get(`${url}`, {
+        let res = await axios.get(`https://api-football-v1.p.rapidapi.com/v2/players/player/${player.player_id}`, {
           "headers": {
             "x-rapidapi-key": config.api_key,
             "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
             "useQueryString": true
           }
       })
-        console.log(res) 
+        console.log(res.data.api.players[0].rating) 
         }
       catch(e){
         console.log(e);
       }
     }
-    getData();
+    //getData();
   }, [])
 
   return (
